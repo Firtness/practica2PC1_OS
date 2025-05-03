2f5572c65f9b7cdfd1691a7a869eed298714f856
Firtness <u202122484@upc.edu.pe>
2025-05-03 11:05:48 -0500

    fix(final): Fix everything 2

--- src/app/public/footer-content/footer-content.component.ts
+++ src/app/public/footer-content/footer-content.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
+import {LanguageSwitcherComponent} from '../language-switcher/language-switcher.component';
 
 @Component({
   selector: 'app-footer-content',
   imports: [
-    TranslatePipe
+    TranslatePipe,
+    LanguageSwitcherComponent
   ],
   templateUrl: './footer-content.component.html',
   styleUrl: './footer-content.component.css'
